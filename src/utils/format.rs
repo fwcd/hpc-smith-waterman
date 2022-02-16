0e5403d9035bd92acb56a9898454c1a5b49aa770
fwcd <fwcdmail@gmail.com>
2022-02-16 16:43:24 +0100

    Create even fancier boxes

--- src/utils/format.rs
+++ src/utils/format.rs
@@ -1,5 +1,5 @@
 /// Creates an ASCII-art-esque box around a string.
 pub fn pretty_box(s: &str) -> String {
-    let gutter = "-".repeat(s.len() + 2);
-    format!("+{}+\n| {} |\n+{}+", gutter, s, gutter)
+    let gutter = "─".repeat(s.len() + 2);
+    format!("┌{}┐\n│ {} │\n└{}┘", gutter, s, gutter)
 }
