6dcec09681fd2effc5029f2f33897837d4337402
fwcd <fwcdmail@gmail.com>
2022-02-17 18:18:41 +0100

    Copy database and query to GPU

--- src/engine/opencl.rs
+++ src/engine/opencl.rs
@@ -1,13 +1,9 @@
 use std::sync::{Arc, Mutex};
-use ocl::{ProQue, Buffer, Kernel, SpatialDims, OclPrm};
-use rayon::prelude::*;
+use ocl::{ProQue, Buffer, SpatialDims, OclPrm};
 
-use crate::{model::{Sequence, AlignedPair, AlignedSequence}, metrics::Metrics, utils::UnsafeSlice};
+use crate::{model::{Sequence, AlignedPair}, metrics::Metrics};
 
 use super::{Engine, G_INIT, G_EXT, WEIGHT_IF_EQ};
-
-/// The OpenCL program source code.
-const PROGRAM_SRC: &str = include_str!("program.cl");
 
 /// An engine that computes alignments using the
 /// Smith-Waterman-Algorithm with OpenCL on the
@@ -18,9 +14,15 @@ pub struct OpenCLEngine {
 
 impl Default for OpenCLEngine {
     fn default() -> Self {
+        // The OpenCL program source code.
+        let program_src: String = include_str!("program.cl")
+            .replace("G_EXT", &G_EXT.to_string())
+            .replace("G_INIT", &G_INIT.to_string())
+            .replace("WEIGHT_IF_EQ", &WEIGHT_IF_EQ.to_string());
+
         // Create the program, queue and context.
         let pro_que = ProQue::builder()
-            .src(PROGRAM_SRC)
+            .src(program_src)
             .build()
             .unwrap();
 
@@ -52,10 +54,16 @@ impl Engine for OpenCLEngine {
         let size = height * width;
 
         // Allocate buffers on the GPU.
+        let gpu_database: Buffer<u8> = self.make_gpu_buffer(n);
+        let gpu_query: Buffer<u8> = self.make_gpu_buffer(m);
         let gpu_h: Buffer<i16> = self.make_gpu_buffer(size);
         let gpu_e: Buffer<i16> = self.make_gpu_buffer(size);
         let gpu_f: Buffer<i16> = self.make_gpu_buffer(size);
         let gpu_p: Buffer<usize> = self.make_gpu_buffer(size);
+
+        // Copy database and query to GPU.
+        gpu_database.write(&database.raw).enq().unwrap();
+        gpu_query.write(&query.raw).enq().unwrap();
 
         for k in 2..=(n + m) {
             // The lower and upper bounds for the diagonal
@@ -66,11 +74,11 @@ impl Engine for OpenCLEngine {
 
             // Create the kernel.
             let kernel = self.pro_que.kernel_builder("smith_waterman_diagonal")
-                .arg(G_EXT)
-                .arg(G_INIT)
                 .arg(k)
                 .arg(width)
                 .arg(lower)
+                .arg(&gpu_database)
+                .arg(&gpu_query)
                 .arg(&gpu_h)
                 .arg(&gpu_e)
                 .arg(&gpu_f)
--- src/engine/program.cl
+++ src/engine/program.cl
@@ -1,11 +1,11 @@
 // Computes a diagonal slice of the Smith-Waterman matrices on the GPU.
 // Mostly a translation of the inner loop from the diagonal engine.
 __kernel void smith_waterman_diagonal(
-    int g_ext,
-    int g_init,
     int k,
     int width,
     int lower,
+    __global uchar *database,
+    __global uchar *query,
     __global int *h,
     __global int *e,
     __global int *f,
@@ -21,21 +21,21 @@ __kernel void smith_waterman_diagonal(
     int above_left = (i - 1) * width + j - 1;
 
     // Compute helper values
-    int e_here = max(e[left] - g_ext, h[left] - g_init);
-    int f_here = max(f[above] - g_ext, h[above] - g_init);
+    int e_here = max(e[left] - G_EXT, h[left] - G_INIT);
+    int f_here = max(f[above] - G_EXT, h[above] - G_INIT);
 
     e[here] = e_here;
     f[here] = f_here;
 
     // Compute value and remember the index the maximum came from
     // (we need this later for the traceback phase)
-    int h_above_left = h[above_left];
+    int from_above_left = h[above_left] + (database[i - 1] == query[j - 1] ? WEIGHT_IF_EQ : -WEIGHT_IF_EQ);
     int max_origin = 0;
     int max_value = 0;
     
-    if (h_above_left > max_value) {
+    if (from_above_left > max_value) {
         max_origin = above_left;
-        max_value = h_above_left;
+        max_value = from_above_left;
     }
 
     if (e_here > max_value) {
