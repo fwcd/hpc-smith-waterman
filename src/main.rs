c8758214b5ac23dbc1f1bb45ab758027d4c8ca8a
fwcd <fwcdmail@gmail.com>
2022-02-21 16:13:02 +0100

    Add a proper subcommand interface

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod metrics;
 mod model;
 mod utils;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use std::{io::{BufReader, self, Write}, fs::File, sync::{Mutex, Arc}};
 use rayon::prelude::*;
 
@@ -56,6 +56,60 @@ fn bench_parallel<'a>(engine: &(impl Engine + Sync), database: &'a Sequence, que
     aligns
 }
 
+#[derive(Parser)]
+#[clap(version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Runs the engines once on a pair of sequences.
+    Run {
+        /// The database sequence.
+        #[clap(default_value = "TGTTACGG")]
+        database: String,
+        /// The query sequence.
+        #[clap(default_value = "GGTTGACTA")]
+        query: String,
+    },
+    /// Benchmarks the different engines.
+    Bench {
+        /// The path to the downloaded dataset.
+        #[clap(short, long, default_value = "data/uniprot_sprot.fasta")]
+        path: String,
+
+        /// The maximum number of sequences to benchmark against.
+        #[clap(short, long, default_value_t = 10_000)]
+        number: usize,
+
+        /// How many times to repeat/cycle each sequence.
+        #[clap(short, long, default_value_t = 1)]
+        repeats: usize,
+
+        /// Whether to benchmark the naive (CPU) engine.
+        #[clap(long)]
+        naive: bool,
+
+        /// Whether to benchmark the diagonal (CPU) engine.
+        #[clap(long)]
+        diagonal: bool,
+
+        /// Whether to benchmark the cache-optimized diagonal (CPU) engine.
+        #[clap(long)]
+        optimized_diagonal: bool,
+
+        /// Whether to benchmark the diagonal OpenCL (GPU) engine.
+        #[clap(long)]
+        opencl_diagonal: bool,
+
+        /// Whether to benchmark the cache-optimized diagonal OpenCL (GPU) engine.
+        #[clap(long)]
+        optimized_opencl_diagonal: bool,
+    },
+}
+
 #[derive(Parser, Debug)]
 #[clap(version, about)]
 struct Args {
@@ -63,37 +117,7 @@ struct Args {
     #[clap(short, long)]
     demo: bool,
 
-    /// The path to the downloaded dataset.
-    #[clap(short, long, default_value = "data/uniprot_sprot.fasta")]
-    path: String,
-
-    /// The maximum number of sequences to benchmark against.
-    #[clap(short, long, default_value_t = 10_000)]
-    number: usize,
-
-    /// How many times to repeat/cycle each sequence.
-    #[clap(short, long, default_value_t = 1)]
-    repeats: usize,
-
-    /// Whether to benchmark the naive (CPU) engine.
-    #[clap(long)]
-    naive: bool,
-
-    /// Whether to benchmark the diagonal (CPU) engine.
-    #[clap(long)]
-    diagonal: bool,
-
-    /// Whether to benchmark the cache-optimized diagonal (CPU) engine.
-    #[clap(long)]
-    optimized_diagonal: bool,
-
-    /// Whether to benchmark the diagonal OpenCL (GPU) engine.
-    #[clap(long)]
-    opencl_diagonal: bool,
-
-    /// Whether to benchmark the cache-optimized diagonal OpenCL (GPU) engine.
-    #[clap(long)]
-    optimized_opencl_diagonal: bool,
+    
 
     /// The index of the GPU to use for the OpenCL engine.
     #[clap(long, default_value_t = 0)]
@@ -102,67 +126,66 @@ struct Args {
 
 fn main() {
     // Parse CLI args
-    let args = Args::parse();
-    let default = !args.demo
-        && !args.naive
-        && !args.diagonal
-        && !args.optimized_diagonal
-        && !args.opencl_diagonal
-        && !args.optimized_opencl_diagonal;
+    let cli = Cli::parse();
 
     // Create engines
     let naive_engine = NaiveEngine;
     let diagonal_engine = DiagonalEngine;
     let optimized_diagonal_engine = OptimizedDiagonalEngine;
-    let opencl_diagonal_engine = OpenCLDiagonalEngine::new(args.gpu_index);
-    let optimized_opencl_diagonal_engine = OptimizedOpenCLDiagonalEngine::new(args.gpu_index);
+    let opencl_diagonal_engine = OpenCLDiagonalEngine::new(0);
+    let optimized_opencl_diagonal_engine = OptimizedOpenCLDiagonalEngine::new(0);
 
-    // Run short demo if --demo is set
-    if args.demo || default {
-        // let demo_database = "TGTTACGG".parse().unwrap();
-        // let demo_query = "GGTTGACTA".parse().unwrap();
-        let demo_database = "HSDLGWKFTPL".parse().unwrap();
-        let demo_query = "SLTTTPSLTTTPPTSLG".parse().unwrap();
-        run(&optimized_diagonal_engine, &demo_database, &demo_query);
-        run(&optimized_opencl_diagonal_engine, &demo_database, &demo_query);
-    }
+    match cli.command {
+        Command::Run { database, query } => {
+            let database = database.parse().unwrap();
+            let query = query.parse().unwrap();
 
-    // Read a subset of the sequences from the downloaded dataset
-    let file = File::open(args.path).expect("Could not open dataset (did you specify --path?)");
-    let mut reader = FastaReader::new(BufReader::new(file)).map(|x| x.cycle(args.repeats));
-    let database = reader.next().unwrap();
-    let queries = reader.take(args.number).collect();
+            run(&naive_engine, &database, &query);
+            run(&diagonal_engine, &database, &query);
+            run(&optimized_diagonal_engine, &database, &query);
+            run(&opencl_diagonal_engine, &database, &query);
+            run(&optimized_opencl_diagonal_engine, &database, &query);
+        },
+        Command::Bench { path, number, repeats, naive, diagonal, opencl_diagonal, optimized_diagonal, optimized_opencl_diagonal } => {
+            let default = !naive && !diagonal && !optimized_diagonal && !opencl_diagonal && !optimized_opencl_diagonal;
+            // Read a subset of the sequences from the downloaded dataset
+            let file = File::open(path).expect("Could not open dataset (did you specify --path?)");
+            let mut reader = FastaReader::new(BufReader::new(file)).map(|x| x.cycle(repeats));
+            let database = reader.next().unwrap();
+            let queries = reader.take(number).collect();
 
-    // Use asserters to verify that engines yield the same result.
-    // Note that the optimized diagonal engines use a different
-    // asserter since they may yield different solutions during
-    // the traceback stage if there are multiple (equivalent) maximums.
-    let mut asserter = EqualAsserter::new();
-    let mut optimized_asserter = EqualAsserter::new();
+            // Use asserters to verify that engines yield the same result.
+            // Note that the optimized diagonal engines use a different
+            // asserter since they may yield different solutions during
+            // the traceback stage if there are multiple (equivalent) maximums.
+            let mut asserter = EqualAsserter::new();
+            let mut optimized_asserter = EqualAsserter::new();
 
-    // Benchmark the naive (CPU) engine
-    if args.naive || default {
-        asserter.feed(bench_sequential(&naive_engine, &database, &queries));
-        asserter.feed(bench_parallel(&naive_engine, &database, &queries));
-    }
+            // Benchmark the naive (CPU) engine
+            if naive || default {
+                asserter.feed(bench_sequential(&naive_engine, &database, &queries));
+                asserter.feed(bench_parallel(&naive_engine, &database, &queries));
+            }
 
-    // Benchmark the diagonal (CPU) engine
-    if args.diagonal || default {
-        asserter.feed(bench_parallel(&diagonal_engine, &database, &queries));
-    }
+            // Benchmark the diagonal (CPU) engine
+            if diagonal || default {
+                asserter.feed(bench_parallel(&diagonal_engine, &database, &queries));
+            }
 
-    // Benchmark the cache-optimized diagonal (CPU) engine
-    if args.optimized_diagonal || default {
-        optimized_asserter.feed(bench_parallel(&optimized_diagonal_engine, &database, &queries));
-    }
+            // Benchmark the cache-optimized diagonal (CPU) engine
+            if optimized_diagonal || default {
+                optimized_asserter.feed(bench_parallel(&optimized_diagonal_engine, &database, &queries));
+            }
 
-    // Benchmark the OpenCL diagonal (GPU) engine
-    if args.opencl_diagonal || default {
-        asserter.feed(bench_parallel(&opencl_diagonal_engine, &database, &queries));
-    }
+            // Benchmark the OpenCL diagonal (GPU) engine
+            if opencl_diagonal || default {
+                asserter.feed(bench_parallel(&opencl_diagonal_engine, &database, &queries));
+            }
 
-    // Benchmark the cache-optimized OpenCL diagonal (GPU) engine
-    if args.optimized_opencl_diagonal || default {
-        optimized_asserter.feed(bench_parallel(&optimized_opencl_diagonal_engine, &database, &queries));
+            // Benchmark the cache-optimized OpenCL diagonal (GPU) engine
+            if optimized_opencl_diagonal || default {
+                optimized_asserter.feed(bench_parallel(&optimized_opencl_diagonal_engine, &database, &queries));
+            }
+        },
     }
 }
